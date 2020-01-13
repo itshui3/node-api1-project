db1a1c91c330e6fd2b6ed270bb85cb84c8cf03be
Hui <jeffrey.x.hui@gmail.com>
2020-01-13 12:11:31 -0800

    root get, users get, user id get written

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+const port = 5000;
+
+server.get('/', (req, res) => {
+  res.status(200).send('Users web API');
+})
+
+server.get('/users', (req, res) => {
+  db.find()
+    .then( resolve => {
+      res.status(200).json(resolve)
+    })
+    .catch( err => {
+      res.status(500).send('Internal server error code 500')
+    })
+})
+
+server.get('/users/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.findById(id)
+    .then( user => {
+      res.status(200).json(user)
+    })
+    .catch( err => {
+      res.status(500).send('Internal server error code 500')
+    })
+})
+
+server.listen(port, () => {
+  console.log(`app listening on port ${port}`);
+})
